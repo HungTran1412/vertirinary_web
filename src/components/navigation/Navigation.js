8a57d021d823ccb23e9ffbdd5176c2ed2108b521
Minh Hùng <hug2k4123@gmail.com>
2025-08-11 22:32:49 +0700

    Navigation bar

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Navigation.scss";
+import {
+    Link,
+    NavLink
+} from "react-router-dom";
+import logo from "../../assets/images/logo.jpg";
+
+
+class Navigation extends React.Component {
+    render() {
+        return (
+            <header className="navbar">
+                <div className="navbar_logo">
+                    <img src={logo} alt="Sakura Pet Clinic & Hotel" />
+                    <NavLink to="/home" activeClassName="active"><span>Sakura Pet Clinic & Hotel</span></NavLink>
+                </div>
+
+                <nav className="navbar__links">
+                    <NavLink to="/about" activeClassName="active">Giới thiệu</NavLink>
+                    <NavLink to="/services" activeClassName="active">Dịch vụ</NavLink>
+                    <NavLink to="/booking" activeClassName="active">Đặt lịch</NavLink>
+                    <NavLink to="/login" className="btn_login">Đăng nhập</NavLink>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default Navigation;
---
+++ src/views/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.scss';
+
+//imprt components
+import Navigation from '../components/navigation/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navigation/>
+       
+        
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
