d7eab5b56d7f079df30434e8831287ebf43d1f1f
Minh Hùng <hug2k4123@gmail.com>
2025-08-12 16:51:12 +0700

    hero section

---
+++ src/views/home/OurServices/OurServices.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class OurServices extends React.Component{
+    render(){
+        return(
+            <>
+                <h3>Các dịch vụ của chúng tôi</h3>
+                <div>
+
+                </div>
+            </>
+        );
+    }
+}
+
+export default OurServices;
